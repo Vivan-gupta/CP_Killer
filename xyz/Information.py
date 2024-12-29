48d5fe835aa2668e5f232dda698f454d92d334f8
Vivan <vivangupt75@gmail.com>
2024-12-29 09:40:08 +0530

    c5

---
+++ xyz/Information.py
@@ -0,0 +1,26 @@
+platforms = ['LeetCode', 'CodeForces', 'CodeChef', 'GeeksForGeeks', 'AtCoder']
+languages=['python', 'java', 'c++']
+extensions={'java': '.java', 'python': '.py', 'c++': '.cpp'}
+file_location = {
+                 'java':{
+                            'LeetCode': 'src/main/java/Solution',
+                            'GeeksForGeeks': 'src/main/java/Solution',
+                            'CodeForces': 'src/main/java/Main',
+                            'CodeChef': 'src/main/java/Main',
+                            'AtCoder': 'src/main/java/Main'
+                        },
+                 'python':{
+                            'LeetCode': 'src/main/python/Solution',
+                            'GeeksForGeeks': 'src/main/python/Solution',
+                            'CodeForces': 'src/main/python/Main',
+                            'CodeChef': 'src/main/python/Main',
+                            'AtCoder': 'src/main/python/Main'
+                          },
+                 'C++':{
+                        'LeetCode': 'src/main/c++/Solution',
+                        'GeeksForGeeks': 'src/main/c++/Solution',
+                        'CodeForces': 'src/main/c++/Main',
+                        'CodeChef': 'src/main/c++/Main',
+                        'AtCoder': 'src/main/c++/Main'
+                       },
+                }
